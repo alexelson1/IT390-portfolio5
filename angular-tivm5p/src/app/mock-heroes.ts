9449af044b9c1deca2d40cd4db818f132d198dc4
alexelson1 <123711494+alexelson1@users.noreply.github.com>
2023-03-02 00:57:35 -0500

    first commit

---
+++ angular-tivm5p/src/app/mock-heroes.ts
@@ -0,0 +1,41 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Mar 01' },
+  { id: 2, name: 'Mar 02' },
+  { id: 3, name: 'Mar 03' },
+  { id: 4, name: 'Mar 04' },
+  { id: 5, name: 'Mar 05' },
+  { id: 6, name: 'Mar 06' },
+  { id: 7, name: 'Mar 07' },
+  { id: 8, name: 'Mar 08' },
+  { id: 9, name: 'Mar 09' },
+  { id: 10, name: 'Mar 10' },
+  { id: 11, name: 'Mar 11' },
+  { id: 12, name: 'Mar 12' },
+  { id: 13, name: 'Mar 13' },
+  { id: 14, name: 'Mar 14' },
+  { id: 15, name: 'Mar 15' },
+  { id: 16, name: 'Mar 16' },
+  { id: 17, name: 'Mar 17' },
+  { id: 18, name: 'Mar 18' },
+  { id: 19, name: 'Mar 19' },
+  { id: 20, name: 'Mar 20' },
+  { id: 21, name: 'Mar 21' },
+  { id: 22, name: 'Mar 22' },
+  { id: 23, name: 'Mar 23' },
+  { id: 24, name: 'Mar 24' },
+  { id: 25, name: 'Mar 25' },
+  { id: 26, name: 'Mar 26' },
+  { id: 27, name: 'Mar 27' },
+  { id: 28, name: 'Mar 28' },
+  { id: 29, name: 'Mar 29' },
+  { id: 30, name: 'Mar 30' },
+  { id: 31, name: 'Mar 31' },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
